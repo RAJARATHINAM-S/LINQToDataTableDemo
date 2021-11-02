07132227a54409fe9b0c328fe49cdcfe24db65fc
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-11-02 08:50:58 +0530

    Add project files.

---
+++ LINQToDataTableDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LINQToDataTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Table demo");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddToDataTableDemo();
+            Console.ReadKey();
+        }
+    }
+}
